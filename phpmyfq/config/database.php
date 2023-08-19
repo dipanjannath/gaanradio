8c94acdedf9fe9cb9960a4d2d49e3adf7fbfa814
rapidmart <43409651+rapidmart@users.noreply.github.com>
2023-08-19 20:11:09 +0530

    updated

---
+++ phpmyfq/config/database.php
@@ -0,0 +1,7 @@
+<?php
+$DB['server'] = 'mysql';
+$DB['user'] = 'u867579236_badev';
+$DB['password'] = 'PypumaGume';
+$DB['db'] = 'u867579236_geqyg';
+$DB['prefix'] = 'twxt_';
+$DB['type'] = 'mysqli';
